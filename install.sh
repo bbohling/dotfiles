efe9bfc617209b1ef26aed73dab394fb41d3e4e2
Brandon Bohling <brandon.bohling@intel.com>
2017-06-14 22:08:02 -0700

    remove fish stuff

--- install.sh
+++ install.sh
@@ -7,24 +7,16 @@ if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# Install NVM
+# TODO: verify that this is the latest version
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
 # Update Homebrew recipes
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle
-
-# Make ZSH the default shell environment
-chsh -s $(which fish)
-
-# Check for Oh My Fish and install if we don't have it
-if test ! $(which omg); then
-  curl -L https://get.oh-my.fish | fish
-fi
-
-# Install Oh My Fish Plugins
-omf install brew
-omf install nvm
 
 # Install global NPM packages
 npm i -g david
@@ -34,7 +26,6 @@ npm i -g pageres
 npm i -g slow-deps
 npm i -g speed-test
 npm i -g yarn
-
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
