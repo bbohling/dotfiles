08824a84addbb948f8cc4c37ebd190e39a3df409
bbohling <brandon@bohling.me>
2022-03-23 16:19:35 -0700

    remove exports

--- fresh.sh
+++ fresh.sh
@@ -22,9 +22,6 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Create an .nvm directory before installing nvm via brew
 mkdir $HOME/.nvm
 
-# symlink dotfiles
-ln -s $HOME/.dotfiles/.exports $HOME/.exports
-
 # Update Homebrew recipes
 brew update
 
